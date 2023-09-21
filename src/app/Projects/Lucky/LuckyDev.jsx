46a9f8c2490f930b63a6cdc672c70054578569ea
BigSzy <szymon.kaluza@outlook.com>
2023-09-21 07:35:39 +0100

    lucky page grid changes

--- src/app/Projects/Lucky/LuckyDev.jsx
+++ src/app/Projects/Lucky/LuckyDev.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import styles from './LuckyDev.module.scss'
+import React from "react";
+import styles from "./LuckyDev.module.scss";
 
 function LuckyDev() {
   return (
-    <div className='container'>
-        <div className={styles.sectionWrapper}>
-            <div className={styles.gridContainer}>
-                <div className={styles.gridItem}>
-                    <img src="/img/lucky/Poses.png" alt="" />
-                </div>
-                <div className={styles.gridItem}>
-                    <img src="/img/lucky/Faces.png" alt="" />
-                </div>
-            </div>
-            <div className={styles.gifWrapper}>
-                <img src="/img/lucky/LuckyGif.gif" alt="" />
-            </div>
+    <div className="container">
+      <div className={styles.gridContainer}>
+        <div className={styles.gridItem1}>
+          <img src="/img/lucky/Poses.png" alt="" />
         </div>
+        <div className={styles.gridItem2}>
+          <img src="/img/lucky/Faces.png" alt="" />
+        </div>
+        <div className={styles.gridItem3}>
+          <img src="/img/lucky/LuckyGif.gif" alt="" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LuckyDev
+export default LuckyDev;
